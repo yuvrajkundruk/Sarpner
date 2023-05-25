36281f5b179869cfd3db904c941e6fafb4451bd9
yuvrajkundruk <76276969+yuvrajkundruk@users.noreply.github.com>
2023-05-25 23:03:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,8 +11,14 @@ function store(){ //stores items in the localStorage
   window.localStorage.setItem(key,JSON.stringify(car));  
   //converting object to string
 }
+function removeItem(){ //deletes item from localStorage
+  var key = document.getElementById('removeKey').value; //gets key from user
+  localStorage.removeItem(key) //passes key to the removeItem method
+  console.log("remove items");
+}
 
 window.onload =function(){ //ensures the page is loaded before functions are executed.
   document.getElementById("carForm").onsubmit = store
+  document.getElementById("removeButton").onclick = removeItem
   
 }
